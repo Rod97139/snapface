5f3ceadbe31d4bae770c477f9185237d4ca5bb4b
Rod97139 <fayaflame@gmail.com>
2023-10-07 18:25:38 +0200

    *ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,26 +12,29 @@ export class AppComponent {
   mySnap2!: FaceSnap;
   mySnap3!: FaceSnap;
   ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit !',
-      new Date(),
-      0,
-      "https://images.unsplash.com/photo-1525869916826-972885c91c1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
-    );
-    this.mySnap2 = new FaceSnap(
-      'Archibald2',
-      'Mon meilleur ami depuis tout petit !',
-      new Date(),
-      0,
-      "https://images.unsplash.com/photo-1525869916826-972885c91c1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
-    );
-    this.mySnap3 = new FaceSnap(
-      'Archibald3',
-      'Mon meilleur ami depuis tout petit !',
-      new Date(),
-      0,
-      "https://images.unsplash.com/photo-1525869916826-972885c91c1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
-    );
+    this.mySnap = {
+      title: 'Archibald',
+      description: 'Mon meilleur ami depuis tout petit !',
+      createdDate: new Date(),
+      snaps: 0,
+      imageUrl: "https://images.unsplash.com/photo-1525869916826-972885c91c1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80",
+      location: "Paris"
+    };
+    this.mySnap2 = {
+      title: 'Archibald2',
+      description: 'Mon meilleur ami depuis tout petit !',
+      createdDate: new Date(),
+      snaps: 0,
+      imageUrl: "https://images.unsplash.com/photo-1525869916826-972885c91c1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80",
+      location: "Montpellier"
+    };
+    this.mySnap3 = {
+      title: 'Archibald3',
+      description: 'Mon meilleur ami depuis tout petit !',
+      createdDate: new Date(),
+      snaps: 0,
+      imageUrl: "https://images.unsplash.com/photo-1525869916826-972885c91c1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
+    };
+
   }
 }
--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,7 +1,10 @@
 export class FaceSnap {
-  constructor(public title: string,
-              public description: string,
-              public createdDate: Date,
-              public snaps: number,
-              public imageUrl: string) {}
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  location?: string;
 }
+
+
