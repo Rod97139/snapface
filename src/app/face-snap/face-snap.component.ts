d402cc0ee63942002a1baece134f26f631a7188a
Rod97139 <fayaflame@gmail.com>
2023-10-07 16:59:45 +0200

    like unlike

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -12,6 +12,7 @@ export class FaceSnapComponent {
   createdDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  buttonText!: string;
 
   ngOnInit() {
     this.title = 'Archibald';
@@ -19,6 +20,17 @@ export class FaceSnapComponent {
     this.createdDate = new Date();
     this.snaps = 6;
     this.imageUrl =  "https://images.unsplash.com/photo-1525869916826-972885c91c1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
+    this.buttonText = 'Oh Snap !';
+  }
+
+  onSnap() {
+    if (this.buttonText === 'Oh Snap !') {
+      this.snaps++;
+      this.buttonText = 'Oops, unSnap !';
+    } else {
+      this.buttonText = 'Oh Snap !';
+      this.snaps--;
+    }
   }
 
 }
