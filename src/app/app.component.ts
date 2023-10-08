c48a5e3fb3a0d5d0c7da0678ea654fe7ce4cef29
Rod97139 <fayaflame@gmail.com>
2023-10-08 18:42:59 +0200

    Observable async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {interval, Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  ngOnInit() {}
+
+  interval$!: Observable<number>; // <number> is the type of the value emitted by the observable
+  ngOnInit() {
+    this.interval$ = interval(1000);
+    // setTimeout(() => this.interval$.subscribe( value => console.log(value)), 3000);
+  }
 }
