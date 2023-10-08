f3d7fa0700b1eda26ef718e672cbc469eac65c74
Rod97139 <fayaflame@gmail.com>
2023-10-08 19:00:18 +0200

    opérateur bas niveau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {interval, Observable} from "rxjs";
+import {filter, interval, map, Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,16 @@ import {interval, Observable} from "rxjs";
 })
 export class AppComponent implements OnInit{
 
-  interval$!: Observable<number>; // <number> is the type of the value emitted by the observable
+  interval$!: Observable<string>; // <number> is the type of the value emitted by the observable
   ngOnInit() {
-    this.interval$ = interval(1000);
-    // setTimeout(() => this.interval$.subscribe( value => console.log(value)), 3000);
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ? `Je suis ${value} et je suis pair` : `Je suis ${value} et je suis impair`),
+      tap(value => this.logger(value))
+    );
+  }
+
+  logger(text: string) {
+    console.log('logger', text);
   }
 }
